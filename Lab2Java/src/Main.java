bf3378ecbd18532ff575509ae778bdf66232aef7
Artechk <kabachenko.81@gmail.com>
2024-04-30 00:34:32 +0300

    fixed

--- Lab2Java/src/Main.java
+++ Lab2Java/src/Main.java
@@ -5,12 +5,11 @@ public class Main {
         int threadNum = 6; // змінено на 4 потока для прикладу
         ArrClass arrClass = new ArrClass(dim, threadNum);
 
-        // Замеряем время выполнения
+        // Заміряемо час виконання, дле порівняння програм
         long startTime = System.nanoTime();
         int min = arrClass.threadMin();
         long endTime = System.nanoTime();
 
-        // Переводим время выполнения в миллисекунды
         double durationMilliseconds = (endTime - startTime) / 1e6;
 
         System.out.println("Мінімальний елемент: " + min);
